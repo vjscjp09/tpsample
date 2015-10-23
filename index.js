020deb6b49085ca9d82ff4b2db6843e9f2a75252
vjscjp09 <vj.scjp009@gmail.com>
2015-10-23 11:20:50 -0400

    Updated Routing

--- index.js
+++ index.js
@@ -17,11 +17,30 @@ app.post('/', function(req, res){
 	// Create a new instance of the TropoWebAPI object.
 	var tropo = new tropowebapi.TropoWebAPI();
 	// Use the say method https://www.tropo.com/docs/webapi/say.htm
-	tropo.say("Hello! Welcome to shipped tropo demo.");
-
+	tropo.say("Shipped !, Welcome to my Tropo Web API node demo.");
+	// Use the on method https://www.tropo.com/docs/webapi/on.htm
+	tropo.on("continue", null, "/one", true);
+	
     res.send(tropowebapi.TropoJSON(tropo));
 });
 
+app.post('/one', function(req, res){
+	// Create a new instance of the TropoWebAPI object.
+	var tropo = new tropowebapi.TropoWebAPI();
+	tropo.say("Hello from resource one!");
+	tropo.on("continue", null, "/two", true);
+	
+	res.send(tropowebapi.TropoJSON(tropo));
+});
+
+app.post('/two', function(req, res){
+	// Create a new instance of the TropoWebAPI object.
+	var tropo = new tropowebapi.TropoWebAPI();
+	tropo.say("Hello from resource two!");
+	tropo.say("Well, enough of that. Goodbye.");
+	
+	res.send(tropowebapi.TropoJSON(tropo));
+});
 
 app.listen(3000);
 console.log('Server running on http://0.0.0.0:3000/')
